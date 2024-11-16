bcdb9e8c37c4e0d2707d2d97ba7fa4d0dc5e08e9
Afkarnir <afkarnir@outlook.com>
2024-11-16 01:11:48 +0100

    feat: add game card component

---
+++ src/app/features/home/game-card/game-card.component.ts
@@ -0,0 +1,30 @@
+import { Component, input } from '@angular/core';
+import { Game, Score } from '../../../core/models/game';
+import { TitleCasePipe } from '@angular/common';
+import { PlayerState } from '../../../core/states/player/player.state';
+import { select } from '@ngxs/store';
+import { IconComponent } from '../../../shared/components/icon/icon.component';
+
+@Component({
+  selector: 'app-game-card',
+  standalone: true,
+  imports: [
+    TitleCasePipe,
+    IconComponent,
+  ],
+  templateUrl: './game-card.component.html',
+  styleUrl: './game-card.component.scss'
+})
+export class GameCardComponent {
+  game = input.required<Game>();
+
+  players = select(PlayerState.getPlayers);
+
+  getPlayerName(playerId: number) {
+    return this.players().find(player => player.id === playerId)?.name;
+  }
+
+  isWinner(playerScore: number, scores: Score[]) {
+    return scores.findIndex(score => playerScore < score.score) === -1;
+  }
+}
